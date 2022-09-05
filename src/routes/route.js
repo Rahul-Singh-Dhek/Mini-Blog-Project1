29413e59e540e96d332df9faa53a808c4e9b285e
alok <alokpatle123@gmail.com>
2022-09-05 17:43:38 +0530

    post

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authorController=require('../controller/authorController')
 
-router.post('/createData',authorController.createAuthor)
+router.post('/authors',authorController.createAuthor)
 
 
 module.exports = router;
