41e2b74f3ce09508ed511680ada52a0afbda1ed7
Gautam <gautamdhakate1234@gmail.com>
2022-09-05 17:33:10 +0530

    update Validation on createAuthor

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,21 +1,24 @@
 const authorModels = require("../Models/authorModels");
 
-let createAuthor=async function(req,res){
-   try{ 
-    let data = req.body;
-    console.log(data)
+let createAuthor = async function (req, res) {
+    try {
 
-    if(Object.keys(data).length==0){
-        res.status().send({msg : "AuthorDetail is must be present", status : false})
-    }
-    if(!(/^[a-z_]{3,}@[a-z]{3,}[.]{1}[a-z.]{2,6}$/).test(email)){
-        res.status().send({msg : `${email} is invalid`, status : false})
-    }
+        let data = req.body;
+        console.log(data)
 
-    let createdData=await authorModels.create(data);
-    res.status(201).send({createdAuthor})}
-    catch(error){
-      res.status(501).send({error})
+        if (Object.keys(data).length == 0) {
+            return res.send({ msg: "AuthorDetail is must be present", status: false })
+        }
+        if (!(/^[a-z0-9_]{3,}@[a-z]{3,}[.]{1}[a-z.]{2,6}$/).test(data.email)) {
+            return res.send({ msg: `${data.email} is invalid`, status: false })
+        }
+
+        let createdData = await authorModels.create(data);
+        res.status(201).send({ data: createdData, status: true })
+    } catch (err) {
+        res.send({ msg: err.message, status: false })
     }
 }
-module.exports.createAuthor=createAuthor;
+
+
+module.exports.createAuthor = createAuthor
