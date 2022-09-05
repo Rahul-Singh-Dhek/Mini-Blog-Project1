126003c80cb5ec0896dae0fb3974f830566c6e86
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-09-05 20:37:29 +0530

    newValidator

--- src/controller/blogsController.js
+++ src/controller/blogsController.js
@@ -1,14 +1,15 @@
 const blogsModels = require("../Models/blogsModels.js");
 const authorModels = require("../Models/authorModels");
-
-
 let createBlogs = async function (req, res) {
 
-    try {
+    // try {
 
         let data = req.body
         if(Object.keys(data).length==0){
             return res.send({msg : "Blog details is must be present",status : false})
+        }
+        if(data.isPublished==true){
+            data.publishedAt=Date.now(); 
         }
         let author = await authorModels.findById(data.authorId)
         if (!author) {
@@ -21,12 +22,12 @@ let createBlogs = async function (req, res) {
             return res.send({msg : "subcategory must be Array",status :false})
         }
         let result = await blogsModels.create(data);
-        res.send({ data: result, status: true })
+        res.send({ data: result, status: true }) 
     }
-    catch (error) {
-        res.status(500).send({ msg: error.message })
-    }
-}
+    // catch (error) {
+    //     res.status(500).send({ msg: error.message })
+    // }
+// }
 module.exports.createBlogs = createBlogs
 
 
