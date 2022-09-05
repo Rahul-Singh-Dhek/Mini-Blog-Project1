c0907ecb52b01e827e5518b364afaa2d7eecd011
Gautam <gautamdhakate1234@gmail.com>
2022-09-05 17:04:41 +0530

    update authorController

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,11 +1,21 @@
 const authorModels = require("../Models/authorModels");
 
 let createAuthor=async function(req,res){
-   try{ let data=req.body;
+   try{ 
+    let data = req.body;
+    console.log(data)
+
+    if(Object.keys(data).length==0){
+        res.status().send({msg : "AuthorDetail is must be present", status : false})
+    }
+    if(!(/^[a-z_]{3,}@[a-z]{3,}[.]{1}[a-z.]{2,6}$/).test(email)){
+        res.status().send({msg : `${email} is invalid`, status : false})
+    }
+
     let createdData=await authorModels.create(data);
     res.status(201).send({createdAuthor})}
     catch(error){
-res.status(501).error({error})
+      res.status(501).send({error})
     }
 }
 module.exports.createAuthor=createAuthor;
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authorController=require('../controller/authorController')
 
-router.post('createData',authorController.createAuthor)
+router.post('/createData',authorController.createAuthor)
 
 
 module.exports = router;
