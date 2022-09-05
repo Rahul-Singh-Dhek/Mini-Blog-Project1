481262d78b66736b9ae1bc121fcfb87add79cec3
alok <alokpatle123@gmail.com>
2022-09-05 21:46:17 +0530

    updated

--- src/controller/blogsController.js
+++ src/controller/blogsController.js
@@ -2,23 +2,28 @@ const blogsModels = require("../Models/blogsModels.js");
 const authorModels = require("../Models/authorModels");
 
 
+// ===========================================CreatingBlog=================================================================
+
 let createBlogs = async function (req, res) {
 
     try {
 
         let data = req.body
-        if(Object.keys(data).length==0){
-            return res.send({msg : "Blog details is must be present",status : false})
+        if (Object.keys(data).length == 0) {
+            return res.send({ msg: "Blog details is must be present", status: false })
         }
         let author = await authorModels.findById(data.authorId)
         if (!author) {
             return res.send({ msg: "athorId is not valid", status: false })
         }
-        if(!Array.isArray(data.tags)){
-            return res.send({msg : "Tags must be Array",status :false})
+        if (!Array.isArray(data.tags)) {
+            return res.send({ msg: "Tags must be Array", status: false })           //validating tags must be array
         }
-        if(!Array.isArray(data.subcategory)){
-            return res.send({msg : "subcategory must be Array",status :false})
+        if (!Array.isArray(data.category)) {
+            return res.send({ msg: "Category must be Array", status: false })       //validating category must be array
+        }
+        if (!Array.isArray(data.subcategory)) {
+            return res.send({ msg: "subcategory must be Array", status: false })    //validating subcategory must be array
         }
         let result = await blogsModels.create(data);
         res.send({ data: result, status: true })
